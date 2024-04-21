b363cf3dc86a05adf996938d02df681b9d3b5a92
Mohammad Hamza <mhdkh444@gmail.com>
2024-04-21 17:34:42 +0530

    files

---
+++ src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUser({ username, password})
+
+  };
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {setUser} = useContext(UserContext);
+  return (
+    <div>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
